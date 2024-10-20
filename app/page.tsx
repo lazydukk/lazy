5832474688057387d9d1d53a9c1601ab5483b32d
bdas <bytex07@proton.me>
2024-10-20 22:44:25 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1 className="text-7xl font-bold">lazydukk/home</h1>
+        <p className="text-1xl text-grey-500">pushing for 1000 ELO</p>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
@@ -14,6 +15,14 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           github →
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://woogles.io/profile/lazydukk"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          woogles →
         </a>
       </footer>
     </div>
